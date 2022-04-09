d2364f350711558d3dbbf98970df0f25d188214e
fatihtatoglu <fatih@tatoglu.net>
2022-04-09 18:43:45 +0000

    deploy: bffbd97931d467ef7e07f991907beaa015c5d6fc

---
+++ js/analytics.js
@@ -0,0 +1,20 @@
+// Clarity tracking code for https://blog.tatoglu.net/
+(function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "be90zvaif3");
+
+// Yandex.Metrika counter
+(function (m, e, t, r, i, k, a) {
+    m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+    m[i].l = 1 * new Date(); k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(88224633, "init", {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+});
